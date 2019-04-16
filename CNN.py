fa2747d881b295f7cb4a9b37ce7dfb3bc8866e40
Abudo-S <abudo.mohamed123456789@gmail.com>
2019-04-16 03:42:23 +0200

    Initial commit

---
+++ CNN.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 03:36:37 2019
+
+@author: Dell
+"""
+
+class CNN:
---
+++ KNN.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 03:35:54 2019
+
+@author: Dell
+"""
+
+class KNN:
+    
