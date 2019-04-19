58c1c20483f7263cf14743c6b459a32798f2b9a6
sa3d clay <sa3d.clay@gmail.com>
2019-04-20 01:22:35 +0200

    save images to csv file

--- SVM.py
+++ SVM.py
@@ -1,9 +1,9 @@
 from sklearn import svm
 
-class SVM:
-    SVC = svm.SVC()
+class our_svm:
+    our_svc = svm.SVC()
     
-    def apply_fit_predict(self,df,labels,test):
-       self.SVC.fit(df,labels)
-       print(self.SVC.predict(df))
+    def apply_fit_predict(self, df, labels, test):
+       self.our_svc.fit(df, labels)
+       print(self.our_svc.predict(test))
     
