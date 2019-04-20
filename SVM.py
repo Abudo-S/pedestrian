f0937d35a247d466a95a51844ca7f0f30cb70543
Abudo-S <abudo.mohamed123456789@gmail.com>
2019-04-20 03:01:31 +0200

    Fix_dataframes

--- SVM.py
+++ SVM.py
@@ -1,9 +1,14 @@
 from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 class SVM:
-    SVC = svm.SVC()
+    SVC = svm.SVC(gamma='scale')
+    
     
     def apply_fit_predict(self,df,labels,test):
        self.SVC.fit(df,labels)
-       print(self.SVC.predict(test))
+       return self.SVC.predict(test)
+       
+    def print_accurcy(self,y_test,y_pred):
+        print(accuracy_score(y_test,y_pred))
     
--- pedestrian.py
+++ pedestrian.py
@@ -8,60 +8,80 @@ from SVM import SVM
 c=Image.open("img_00000.pgm")
 print(list(c.getdata()))
 '''
+#4800
+length1=500 
+#5000
+length2=500  
 
 labels = [] 
-images_data = [] 
+images_data =[]
 
 test_labels = []
 images_test_data = []
+features=[]
 
+for i in range(648):
+    features.append("feat"+str(i))      
+
+df1 = pd.DataFrame(columns=features) 
+df2 = pd.DataFrame(columns=features)
 #load_training_dataset
 for i in range(1,4):
-    for j in range(500): #4800
+    for j in range(length1): #4800
         file_dir = '/ped_examples/'
         strr = (str(i) + file_dir)
         
-        img = np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))
-        labels = np.append(labels, 1) #pedestrian
-        images_data = np.append(images_data, img)
-
-    for j in range(500): #5000
+        img = np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))  
+        img = img.flatten()
+        df1.loc[j+((i-1)*length1)]=img
+        images_data=np.append(images_data, img)
+        labels.append(1) #pedestrian
+        
+for i in range(1,4):
+    for j in range(length2): #5000
         file_dir = '/non-ped_examples/'
         strr = (str(i) + file_dir)
         
         img = np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))
-        labels = np.append(labels, 0) #non-pedestrian
-        images_data = np.append(images_data, img)   
+        img = img.flatten()
+        df1.loc[j+((i-1)*length2+(3*length1))]=img
+        images_data=np.append(images_data, img)
+        labels.append(0) #non-pedestrian
      
 #load_testing_dataset
 for i in range(1,3):
-     for j in range(500): #4800
+     for j in range(length1): #4800
          file_dir = "/ped_examples/"
          strr = ('T' + str(i) + file_dir)
          
-         img=np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))
-         test_labels = np.append(labels, 1) #pedestrian
-         images_test_data = np.append(images_test_data, img)
+         img = np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))
+         img = img.flatten()
+         df2.loc[j+((i-1)*length1)]=img
+         images_test_data=np.append(images_test_data, img)
+         test_labels.append(1) #pedestrian
 
-     for j in range(500): #5000
+     for j in range(length2): #5000
          file_dir = "/non-ped_examples/"
          strr = ('T' + str(i) + file_dir)
          
-         img=np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))
-         test_labels = np.append(labels, 0) #non-pedestrian
-         images_test_data = np.append(images_test_data, img)
-         
-df1 = pd.DataFrame(images_data, columns=["data"])
-df2 = pd.DataFrame(images_test_data, columns=["data"])
-
-#print(df1.head())
-#print(df2.head())
+         img = np.array(Image.open(strr + "img_" + "{0:05}".format(j) + ".pgm"))
+         img = img.flatten()
+         df2.loc[j+((i-1)*length2+(2*length1))]=img
+         images_test_data=np.append(images_test_data, img)
+         test_labels.append(1) #non-pedestrian
+            
+images_data = images_data.reshape((length1*3)+(length2*3),648)
+images_test_data = images_test_data.reshape((length1*2)+(length2*2),648)
+#df1 = pd.DataFrame(images_data,columns=features) 
+#print(df1.info)
+#print(df2.info)
 
 #print(labels)
 #print(test_labels)
-svc=SVM()
-print(svc.apply_fit_predict(df1,labels,df2))
 
+svc=SVM()
+y_pred=svc.apply_fit_predict(df1,labels,df2)
+svc.print_accurcy(test_labels,y_pred)
 ##df=pd.DataFrame(images_data,columns=["data"]) 
 #df=pd.DataFrame(images_data,columns=["data"]) 
 #print(df.head())
@@ -71,6 +91,6 @@ print(svc.apply_fit_predict(df1,labels,df2))
 #
 #SVM.fit(df,labels)
 #print(SVM.predict(df))
-print(labels)
-print(test_labels)
+#print(labels)
+#print(test_labels)
 
