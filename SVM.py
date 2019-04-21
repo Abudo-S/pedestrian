f0bfe4b81e1e1a810b13e20e0565b763dce9c152
Abudo-S <abudo.mohamed123456789@gmail.com>
2019-04-21 03:47:22 +0200

    improve_SVM_accuracy

--- SVM.py
+++ SVM.py
@@ -1,16 +1,37 @@
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
 
 class SVM:
-    SVC = svm.SVC(gamma='scale')
-    
-#    def __init__(self,df,labels,test):
-    
+#    c = [0.001, 0.01, 0.1, 1, 10]
+#    gamma = [0.001, 0.01, 0.1, 1]
+    SVC=svm.SVC(gamma='scale')
+    def __init__(self):
+        steps=[('scaler',StandardScaler()),('svm',self.SVC)]
+        self.pipeline=Pipeline(steps)
+
     def apply_fit_predict(self,df,labels,test):
-       self.SVC.fit(df,labels)
-       return self.SVC.predict(test)
-       
+       self.pipeline.fit(df.astype(float),labels)
+       return self.pipeline.predict(test.astype(float))
+   
+#    def __init__(self):
+#        param_grid = {'C': self.c, 'gamma' : self.gamma}
+#        self.grid_search = GridSearchCV(svm.SVC(kernel='rbf'), param_grid, cv=5)
+#          
+#    def apply_grid_search(self,train,labels):
+#        self.grid_search.fit(train, labels)
+#        return self.grid_search.best_params_    # gamma=0.001 , C=0.01
+#        
+#    def apply_fit_predict(self,train,labels,test,param):
+#        self.SVC=svm.SVC(gamma=param['gamma'],C=param['C'])
+#        print(param)
+#        self.SVC.fit(train,labels)
+#        return self.SVC.predict(test)
+    
     def print_accuracy(self,y_test,y_pred):
         print("accuracy:"+str(accuracy_score(y_test,y_pred)))
+        
     
     
